69c5c91466696268a4d60a13fc6e08d96f7c66c2
Zrf11 <vergil1998@hotmail.com>
2020-10-16 19:39:05 +0400

    Update useGoogleSearch.js

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,12 +1,10 @@
 
 import {useState, useEffect} from 'react'
-// const CONTEXT_KEY = 'b1e95dc36f4ace91c'
-// const API_KEY = 'AIzaSyAP9V6vXWaVvf9b5-zlHNC8icZffCiIiOY'
 const useGoogleSearch = (term) => {
     const [data, setdata] = useState(null);
     useEffect(()=>{
     const fetchData = async() => {
-        fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAP9V6vXWaVvf9b5-zlHNC8icZffCiIiOY&cx=b1e95dc36f4ace91c&q=${term}`).then(response => response.json()).then(result => { setdata(result) })
+        fetch(`https://www.googleapis.com/customsearch/v1?key=${enter your API KEY here}&cx=b1e95dc36f4ace91c&q=${term}`).then(response => response.json()).then(result => { setdata(result) })
     }
     fetchData();
     }, [term])
